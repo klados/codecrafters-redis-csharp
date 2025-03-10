5eb0d67d5417d60845973dc3def87b4457b8c655
George Klados <kladgeo@gmail.com>
2025-03-10 21:33:25 +0200

    receive message from client

--- src/Server.cs
+++ src/Server.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
@@ -7,4 +8,12 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-server.AcceptSocket(); // wait for client
+var socket = server.AcceptSocket(); // wait for client
+
+var buffer = new byte[8];
+var bytesReceived = await socket.ReceiveAsync(buffer, SocketFlags.None);
+
+if (bytesReceived > 0)
+{
+    await socket.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);    
+}
