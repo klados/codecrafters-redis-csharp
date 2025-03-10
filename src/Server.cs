489912f22fed684c766ced3d5cae4c211fa0e542
George Klados <kladgeo@gmail.com>
2025-03-10 22:23:09 +0200

    refactor to allow more than one redis-cli request

--- src/Server.cs
+++ src/Server.cs
@@ -4,15 +4,41 @@ using System.Text;
 
 TcpListener server = new TcpListener(IPAddress.Any, 6379);
 server.Start();
-var socket = server.AcceptSocket(); // wait for client
-
 while (true)
 {
-    var buffer = new byte[200];
-    var bytesReceived = await socket.ReceiveAsync(buffer, SocketFlags.None);
+    Console.WriteLine("Client connected");
+    var socket = server.AcceptSocket(); // wait for client
+    Task.Run(() => handleTask(socket));
+}
 
-    if (bytesReceived > 0)
+
+static async Task handleTask(Socket socket)
+{
+    try
     {
-        await socket.SendAsync(Encoding.UTF8.GetBytes("+PONG\r\n"), SocketFlags.None);    
+        while (true)
+        {
+            var buffer = new byte[1024];
+            var bytesReceived = await socket.ReceiveAsync(buffer, SocketFlags.None);
+
+            if (bytesReceived == 0)
+            {
+                break;
+            }
+
+            byte[] pongResponse = Encoding.ASCII.GetBytes("+PONG\r\n");
+
+            await socket.SendAsync(pongResponse, SocketFlags.None);
+
+        }
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine($"An error occurred: {e.Message}");
+    }
+    finally
+    {
+        socket.Close();
+        Console.WriteLine("Client connection closed");
     }
 }
