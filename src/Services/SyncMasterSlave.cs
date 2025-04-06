4bbed90083530f312ee248aab057f20154b7a27a
George Klados <kladgeo@gmail.com>
2025-04-06 12:57:06 +0300

    complete slave handshake for Replication

--- src/Services/SyncMasterSlave.cs
+++ src/Services/SyncMasterSlave.cs
@@ -24,10 +24,32 @@ public class SyncMasterSlave
             {
                 using (NetworkStream stream = client.GetStream())
                 {
+                    var receivedData = new Byte[256];
+                    
                     string pingMessage = "1\r\n$4\r\nPING\r\n";
-                    byte[] data = Encoding.ASCII.GetBytes(BuildResponse.Generate('*', pingMessage));
-                    stream.Write(data, 0, data.Length);
+                    byte[] pingData = Encoding.ASCII.GetBytes(BuildResponse.Generate('*', pingMessage));
+                    stream.Write(pingData, 0, pingData.Length);
                     Console.WriteLine("Ping message sent");
+                    
+                    var bytesRead = stream.Read(receivedData, 0, receivedData.Length);
+                    var responseData = System.Text.Encoding.ASCII.GetString(receivedData, 0, bytesRead);
+                    Console.WriteLine($"master ping response: {responseData}");
+                    
+                    string listeningPort = $"3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n{Config.Port}\r\n";
+                    byte[] listeningPortData = Encoding.ASCII.GetBytes(BuildResponse.Generate('*', listeningPort));
+                    stream.Write(listeningPortData, 0, listeningPortData.Length);
+
+                    bytesRead = stream.Read(receivedData, 0, receivedData.Length);
+                    responseData = System.Text.Encoding.ASCII.GetString(receivedData, 0, bytesRead);
+                    Console.WriteLine($"master replyconf response: {responseData}");
+
+                    string capa = "3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n";
+                    byte[] capaData = Encoding.ASCII.GetBytes(BuildResponse.Generate('*', capa));
+                    stream.Write(capaData, 0, capaData.Length);
+                    
+                    bytesRead = stream.Read(receivedData, 0, receivedData.Length);
+                    responseData = System.Text.Encoding.ASCII.GetString(receivedData, 0, bytesRead);
+                    Console.WriteLine($"master replyconf2 response: {responseData}");
                 }
             }
         }
